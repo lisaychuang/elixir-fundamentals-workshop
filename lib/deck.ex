fd01134d711c2c8fa9245f09a0ef2099e6522686
Lisa Huang <lisa.yc.huang@gmail.com>
2017-04-25 12:46:38 +0200

    Added solution to exercise 11

--- lib/deck.ex
+++ lib/deck.ex
@@ -1,10 +1,10 @@
 defmodule Deck do
-  @deck for suit <- ~w(H C S D),
+  @deck for suit <- ~w(H C S D),  #this shorthand converts into [H,C,S,D]
           card <- ~w(A 2 3 4 5 6 7 8 9 10 J Q K),
           do: "#{card}#{suit}"
+
   def get_cards(num) when num > 0 do
     @deck
-    |> Enum.shuffle
-    |> Enum.take(num)
-  end
+    |> Enum.shuffle #shuffles the list order
+    |> Enum.take(num) #take specific number of cards
 end
