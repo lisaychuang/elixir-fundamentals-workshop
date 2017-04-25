ae6f491ca89003148d9d027d4df06784b77a59a3
Lisa Huang <lisa.yc.huang@gmail.com>
2017-04-25 15:42:07 +0200

    partial solution for exercise 13

--- lib/list_utils.ex
+++ lib/list_utils.ex
@@ -14,6 +14,21 @@ defmodule ListUtils do
 
   # Exercise 13
   def pmap(list, func) do
-    # Add your exercise 13 solution here
-  end
+    parent_pid =self()
+    # 1. Enum.map to turn all of these items into PIDs for spawn_link(fn -> .... end)
+    list
+    |> Enum.map( fn item ->
+      spawn_link(fn ->
+        transformed_item = func.(item)
+        send(parent_pid, transformed_item)
+      end)
+   end )
+
+    # 2. Enum.map again and return "receive blocks" that each wait for a message to be sent back
+    # 3. the function passed to spawn_link in step 1 should first calculate the "transformed value" (with func)
+    #      and then send a message back to the parent, so it can be received in the receive blocks (step 2)
+
+
+    
+end
 end
