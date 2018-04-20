af358789be784282a5bf627e19e0f9ad3a15ee2e
Lisa Huang <lisa.yc.huang@gmail.com>
2018-04-19 17:10:55 -0700

    Added solution to Exercise 7: ListUtils

--- lib/list_utils.ex
+++ lib/list_utils.ex
@@ -1,12 +1,17 @@
 defmodule ListUtils do
   # Exercise 7
-  def reduce(list, reducer, accumulator) do
-    
-    
+  def reduce([h|t], reducer, accumulator) do
+    newAcc = reducer.(h, accumulator)
+    reduce(t, reducer, newAcc)
   end
+  def reduce([], _reducer, accumulator), do: accumulator
+
 
   def map(original, transform_fn, transformed \\ []) do
-    
+    original
+    |> reduce(fn(item, transformed_list)->
+      transformed_list ++ [transform_fn.(item)]
+    end, [])
   end
 
   # Exercise 8
